61631b64a83b64054669d89343062e071764e6e8
User <alexust11@yandex.ru>
2022-12-07 20:47:02 +0700

    Initial commit

---
+++ src/test/java/pro/sky/curse2_2/Curse22ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.curse2_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Curse22ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
